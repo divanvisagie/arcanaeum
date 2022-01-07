f12bc5b4c40832b6f3452f8b3b51b48446843197
Divan Visagie <me@dvisagie.com>
2022-01-07 21:51:44 +0100

    things are working in rust

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
 import { promises as fs } from 'fs'
-import os from 'os'
 import path from 'path'
-import { head, slice } from 'ramda'
-import { buffer } from 'stream/consumers'
-import util from 'util'
 
 const ROOT = `.`
 let FILE_CURSOR_POSITION = 0;
@@ -14,13 +10,16 @@ type SaveGame = {
     saveNumber: number
     header: string
     version: number
+    playerName: string
+    playerLevel: number 
+    playerLocation: string
 }
-type Size = 1 | 8
+type Size = 1 | 4
 type SizeType = {
     size: Size
 }
 const u32: SizeType = {
-    size: 8
+    size: 4
 }
 
 function readBuffer(buffer: Buffer, size: number): Buffer {
@@ -30,14 +29,11 @@ function readBuffer(buffer: Buffer, size: number): Buffer {
     return chunk
 }
 
-function readNumberWithSize(buffer: Buffer, size: Size): number {
-    const slice = readBuffer(buffer, size)
-    return slice.readInt32BE()
-}
-
-function readChars(buffer: Buffer, count: number): string {
-    const slice = readBuffer(buffer, count)
-    return slice.toString()
+function readWstringFromBuffer(buffer: Buffer) {
+    let stringSizeBuf = buffer.slice(FILE_CURSOR_POSITION, FILE_CURSOR_POSITION + 2)
+    const strLen = stringSizeBuf.readInt16LE()
+    FILE_CURSOR_POSITION += 2
+    return readBuffer(buffer, strLen).toString()
 }
 
 async function processFile(filePath: string) {
@@ -47,31 +43,23 @@ async function processFile(filePath: string) {
         headerSize: 0,
         header: '',
         saveNumber: 0,
-        version: 0
+        version: 0,
+        playerName: '',
+        playerLevel: 0,
+        playerLocation: ''
     }
     console.log(`Loading file: ${filePath}`)
     const fileBuffer = await fs.readFile(filePath);
 
     let token_buffer = []
     
-    saveGame.gameTitle = fileBuffer.slice(0,13).toString()
-    saveGame.headerSize = fileBuffer.slice(13,17).readInt32LE()
-    saveGame.version = fileBuffer.slice(17,21).readInt32LE()
-    saveGame.saveNumber = fileBuffer.slice(21,25).readInt32LE()
-
-    fileBuffer.slice()
-
-    // let gameTitle = readChars(buf, 13)
-    // saveGame.gameTitle = gameTitle
-
-    // let headerSizeBuf = readBuffer(buf, u32.size)
-    // saveGame.headerSize = headerSizeBuf.readUInt32LE()
-
-    // // We are now reading from the header
-
-    // let nameWidth = readBuffer(buf, u32.size)
-    // saveGame.saveNumber = nameWidth.readUInt32LE()
-
+    saveGame.gameTitle = readBuffer(fileBuffer, 13).toString()
+    saveGame.headerSize = readBuffer(fileBuffer, 4).readInt32LE()
+    saveGame.version = readBuffer(fileBuffer, 4).readInt32LE()
+    saveGame.saveNumber = readBuffer(fileBuffer, 4).readInt32LE()
+    saveGame.playerName = readWstringFromBuffer(fileBuffer)
+    saveGame.playerLevel = readBuffer(fileBuffer, 4).readInt32LE()
+    saveGame.playerLocation = readWstringFromBuffer(fileBuffer)
 
     console.log(`==================================`)
     console.dir(saveGame)
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use byteorder::{BigEndian, ReadBytesExt};
-use std::io::Cursor;
+/**
+ * https://en.uesp.net/wiki/Skyrim_Mod:Save_File_Format#Header
+ * https://en.uesp.net/wiki/Skyrim_Mod:File_Format_Conventions
+*/
+
+
+use byteorder::{ReadBytesExt, LittleEndian};
+use std::io::{Cursor, Seek};
 use std::{
     io::{self, Read},
     mem::size_of,
@@ -15,29 +21,8 @@ fn read_string_of_size(br: &mut std::fs::File, size: u32) -> String {
     str
 }
 
-// fn read_buffer(br: &mut std::fs::File) -> u32 {
-//     let chunk_size = size_of::<u32>();
-
-//     println!("Reading buffer for {:?} bytes", chunk_size as u64);
-
-//     let mut buffer = Vec::with_capacity(chunk_size);
-
-//     br.take(chunk_size as u64)
-//         .read_to_end(&mut buffer)
-//         .map_err(|err| println!("{:?}", err))
-//         .ok();
-
-//     println!("Read buffer {:?}", buffer);
-
-//     let mut rdr = Cursor::new(buffer);
-//     rdr.read_u32::<BigEndian>().unwrap()
-// }
-
-
 fn read_u32(br: &mut std::fs::File) -> u32 {
     let chunk_size = size_of::<u32>();
-
-    println!("Reading buffer for {:?} bytes", chunk_size as u64);
 
     let mut buffer = Vec::with_capacity(chunk_size);
 
@@ -46,10 +31,32 @@ fn read_u32(br: &mut std::fs::File) -> u32 {
         .map_err(|err| println!("{:?}", err))
         .ok();
 
-    println!("Read buffer {:?}", buffer);
+    let mut rdr = Cursor::new(buffer);
+    rdr.read_u32::<LittleEndian>().unwrap()
+}
+
+fn read_u16(br: &mut std::fs::File) -> u16 {
+    let chunk_size = size_of::<u16>();
+
+    let mut buffer = Vec::with_capacity(chunk_size);
+
+    br.take(chunk_size as u64)
+        .read_to_end(&mut buffer)
+        .map_err(|err| println!("{:?}", err))
+        .ok();
 
     let mut rdr = Cursor::new(buffer);
-    rdr.read_u32::<BigEndian>().unwrap()
+    rdr.read_u16::<LittleEndian>().unwrap()
+}
+
+fn read_wstring(br: &mut std::fs::File) -> String {
+    let size = read_u16(br);
+    let mut str = String::new();
+    br.take(size as u64)
+        .read_to_string(&mut str)
+        .map_err(|err| println!("{:?}", err))
+        .ok();
+    str
 }
 
 fn main() -> io::Result<()> {
@@ -58,6 +65,8 @@ fn main() -> io::Result<()> {
 
     let magic = read_string_of_size(br,13);
     println!("magic: {:?}", magic);
+
+    
 
     let header_size = read_u32(br);
     println!("header size: {:?}", header_size);
@@ -69,10 +78,17 @@ fn main() -> io::Result<()> {
     let save_number = read_u32(br);
     println!("saveNumber {:?}", save_number);
 
-    let player_name = read_string_of_size(br, 12);
+    let player_name = read_wstring(br);
     println!("player name: {:?}", player_name);
-    // let header = read_string(br, header_size);
-    // println!("header size: {:?}", header);
+
+    let player_level = read_u32(br);
+    println!("player level: {:?}", player_level);
+
+    let player_location = read_wstring(br);
+    println!("player location: {:?}", player_location);
+
+    let sp = br.stream_position()?;
+    println!("stream position: {:?}", sp);
 
     Ok(())
 }
