d9433555ca6d8ad592e68af3c14242be8d619bfb
Divan Visagie <me@dvisagie.com>
2022-01-07 23:23:12 +0100

    Implement generic file iterator

--- src/main.rs
+++ src/main.rs
@@ -59,36 +59,84 @@ fn read_wstring(br: &mut std::fs::File) -> String {
     str
 }
 
+#[derive(PartialEq, Eq)]
+enum SkType {
+    SkU32,
+    SKU16,
+    SkWstring,
+    SkChars
+}
+
+struct InfoItem {
+    name: String,
+    size: usize,
+    sk_type: SkType
+}
+
+impl InfoItem {
+    fn new(name: &str, sk_type: SkType) -> InfoItem {
+        InfoItem {
+            name: name.to_string(),
+            size: 0,
+            sk_type
+        }
+    }
+
+    fn new_with_size(name: &str, size: usize, sk_type: SkType) -> InfoItem {
+        InfoItem {
+            name: name.to_string(),
+            size,
+            sk_type
+        }
+    }
+
+    fn print_value(&self,br: &mut std::fs::File){
+        if self.sk_type == SkType::SkChars {
+            let str = read_string_of_size(br,self.size as u32);
+            println!("{}:    {:?}", self.name ,str);
+        }
+        if self.sk_type == SkType::SkU32 {
+            let u  = read_u32(br);
+            println!("{}:    {:?}", self.name, u);
+        }
+
+        if self.sk_type == SkType::SKU16 {
+            let u  = read_u16(br);
+            println!("{}:    {:?}", self.name, u);
+        }
+
+        if self.sk_type == SkType::SkWstring {
+            let str = read_wstring(br);
+            println!("{}:    {:?}", self.name, str);
+        }
+    }
+
+}
+
 fn main() -> io::Result<()> {
     let mut file = std::fs::File::open("test.ess")?;
     let br = file.by_ref();
 
-    let magic = read_string_of_size(br,13);
-    println!("magic: {:?}", magic);
 
-    
+    let items = [
+       InfoItem::new_with_size("magic",13, SkType::SkChars),
+       InfoItem::new("header_size", SkType::SkU32),
+       InfoItem::new("version", SkType::SkU32),
+       InfoItem::new("save_number", SkType::SkU32),
+       InfoItem::new("player_name", SkType::SkWstring),
+       InfoItem::new("player_level", SkType::SkU32),
+       InfoItem::new("player_location", SkType::SkWstring),
+       InfoItem::new("game_date", SkType::SkWstring),
+       InfoItem::new("player_race_editor_id", SkType::SkWstring),
+       InfoItem::new("player_sex", SkType::SKU16)
+    ];
 
-    let header_size = read_u32(br);
-    println!("header size: {:?}", header_size);
-
-    let version = read_u32(br);
-    println!("version {:?}", version);
-
-
-    let save_number = read_u32(br);
-    println!("saveNumber {:?}", save_number);
-
-    let player_name = read_wstring(br);
-    println!("player name: {:?}", player_name);
-
-    let player_level = read_u32(br);
-    println!("player level: {:?}", player_level);
-
-    let player_location = read_wstring(br);
-    println!("player location: {:?}", player_location);
+    for i in items {
+        i.print_value(br);
+    }
 
     let sp = br.stream_position()?;
-    println!("stream position: {:?}", sp);
+    println!("========================\nposition in file: {:?}", sp);
 
     Ok(())
 }
