33ff57e084550d373e8ac16bb3cebfcd45261860
Divan Visagie <me@dvisagie.com>
2022-01-16 17:38:27 +0100

    Remove hard coded path

--- src/mod_search/vortex_scanner.rs
+++ src/mod_search/vortex_scanner.rs
@@ -1,11 +1,6 @@
-use std::fs;
+use std::{env, fs, path::PathBuf};
 
 use serde::{Deserialize, Serialize};
-
-#[allow(dead_code)]
-pub fn read_vortext_mods() {
-    // let path = "C:\\Users\\visag\\AppData\\Roaming\\Vortex\\skyrimse\\mods\\vortex.deployment.msgpack";
-}
 
 #[derive(Clone, Debug)]
 pub struct Plugin {
@@ -28,8 +23,16 @@ struct MasterListFileType {
 
 #[allow(unused)]
 pub fn get_masterlist_data() -> Vec<Plugin> {
-    let path = "C:\\Users\\visag\\AppData\\Roaming\\Vortex\\skyrimse\\masterlist\\masterlist.yaml";
-    let mut file_contents = fs::read_to_string(path).expect("Could not read file");
+    let app_data_path = env::var("APPDATA").unwrap();
+    let mut path_buf = PathBuf::new();
+    path_buf.push(app_data_path);
+    path_buf.push("Vortex");
+    path_buf.push("skyrimse");
+    path_buf.push("masterlist");
+    path_buf.push("masterlist.yaml");
+    println!("Looking for vortex at: {:?}", path_buf);
+   
+    let mut file_contents = fs::read_to_string(path_buf).expect("Could not read file");
     let parsed: MasterListFileType =
         serde_yaml::from_str(file_contents.as_str()).expect("It borked!");
     parsed.plugins.iter().map(|x| parse_plugin(&x)).collect()
