951191a6d240164646f060a1a729e58809c6ea30
Divan Visagie <me@dvisagie.com>
2022-01-26 20:28:46 +0100

    Add u32 read to parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -5,9 +5,10 @@ use skyrim_savegame::SaveFile;
 #[derive(Debug)]
 pub struct SaveInfo {
     pub magic_string: String,
+    pub header_size: u32,
 }
 
-fn read_string(buffer: Vec<u8>, start: usize, end: usize) -> (String, usize) {
+fn read_string(buffer: &[u8], start: usize, end: usize) -> (String, usize) {
     let chunk = &buffer[start..end];
     let s = match std::str::from_utf8(chunk) {
         Ok(s) => s.to_string(),
@@ -19,11 +20,33 @@ fn read_string(buffer: Vec<u8>, start: usize, end: usize) -> (String, usize) {
     (s, end)
 }
 
+fn read_u32(buffer: &[u8], start: usize) -> (u32, usize) {
+    let chunk = &buffer[start..start + 4];
+
+    let n = match <[u8; 4]>::try_from(chunk) {
+        Ok(bytes) => {
+            let n = u32::from_le_bytes(bytes);
+            n
+        }
+        Err(_) => {
+            tracing::error!("Could not parse u32 from chunk at {start}");
+            0
+        }
+    };
+
+    (n, start + 4)
+}
+
 pub fn parse(buf: Vec<u8>) -> SaveInfo {
+    let buf = buf.as_slice();
     let cursor = 0;
     let (magic_string, cursor) = read_string(buf, cursor, 13);
+    let (header_size, cursor) = read_u32(buf, cursor);
     println!("Cursor position at {:?}", cursor);
-    SaveInfo { magic_string }
+    SaveInfo {
+        magic_string,
+        header_size,
+    }
 }
 
 #[cfg(test)]
@@ -46,5 +69,12 @@ mod test {
         let buf = get_file_buffer();
         let save_info = parse(buf);
         assert_eq!(save_info.magic_string, "TESV_SAVEGAME");
+    }
+
+    #[test]
+    fn test_parse_u32() {
+        let buf = get_file_buffer();
+        let save_info = parse(buf);
+        assert_eq!(save_info.header_size, 97);
     }
 }
