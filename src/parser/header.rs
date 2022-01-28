a66348914a3220eac963ec3c9d16f6f6cd052ed7
Divan Visagie <me@dvisagie.com>
2022-01-28 08:25:09 +0100

    Read player sex

--- src/parser/header.rs
+++ src/parser/header.rs
@@ -1,4 +1,21 @@
-use super::utils::{read_u32, read_w_string};
+use super::utils::{read_u16, read_u32, read_w_string};
+
+#[derive(Debug)]
+pub enum Sex {
+    Male,
+    Female,
+    Undefined,
+}
+
+impl From<u16> for Sex {
+    fn from(sex_bit: u16) -> Sex {
+        match sex_bit {
+            0 => Sex::Male,
+            1 => Sex::Female,
+            _ => Sex::Undefined,
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Header {
@@ -9,6 +26,7 @@ pub struct Header {
     pub player_location: String,
     pub game_date: String,
     pub player_race_editor_id: String,
+    pub player_sex: Sex,
 }
 
 pub fn read_header(buf: &[u8], start: usize) -> (Header, usize) {
@@ -19,6 +37,7 @@ pub fn read_header(buf: &[u8], start: usize) -> (Header, usize) {
     let (player_location, cursor) = read_w_string(buf, cursor);
     let (game_date, cursor) = read_w_string(buf, cursor);
     let (player_race_editor_id, cursor) = read_w_string(buf, cursor);
+    let (player_sex_bit, cursor) = read_u16(buf, cursor);
     let header = Header {
         version,
         save_number,
@@ -27,6 +46,7 @@ pub fn read_header(buf: &[u8], start: usize) -> (Header, usize) {
         player_location,
         game_date,
         player_race_editor_id,
+        player_sex: Sex::from(player_sex_bit),
     };
     (header, cursor)
 }
